f2e203b47a32b7e99bf6f64c31005175bce4d4fb
Terrance Rose Jr <trose618@gmail.com>
2018-10-24 18:04:35 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
+require 'pry'
 # Write your code here.
+def word_substituter(tweet)
+  words = tweet.split(" ")
+ shortened_sent = words.collect do |word|
+    if dictionary.keys.include?(word)
+        dictionary[word]
+      else 
+        word
+      end
+    end 
+  #binding.pry
+  shortened_sent.join(" ")
+end
+
+def dictionary
+  {"hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def bulk_tweet_shortener(tweets)
+  puts word_substituter(tweets)
+end
