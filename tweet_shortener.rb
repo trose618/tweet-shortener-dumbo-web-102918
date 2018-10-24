acf7df02abe203f1d299264e79dcf21a3964987c
Terrance Rose Jr <trose618@gmail.com>
2018-10-24 18:17:32 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,12 +35,10 @@ def bulk_tweet_shortener(tweets)
   end
 end
 
-def selective_tweet_shortener(tweets)
-  tweets.each do |tweet|
+def selective_tweet_shortener(tweet)
     if tweet.split("").size > 140
       puts word_substituter(tweet)
     else
       puts tweet
     end
-  end
 end
